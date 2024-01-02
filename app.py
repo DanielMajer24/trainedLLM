ba1ca2e1c23783a68be9babaff0c51ab78db1054
ryanhurst14 <ryanhurst1017@gmail.com>
2024-01-02 20:44:23 +1000

    Added textbox and button

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ st.write("This is a simple streamlit application.")
 
 st.text("This is a text line.")
 
-
+st.text("This is another text line or smth")
+st.button("Click me please")
 # run the application using the following command
 # streamlit run app.py
